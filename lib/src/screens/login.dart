c4cec9a8921ad2318d299349c045c450af78cea1
kattenlaf <ruelgordon123@gmail.com>
2019-11-17 14:26:49 -0500

    3 - Login Screen, assets directory added

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -7,9 +7,50 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: Center(
-        child: submitButton(context),
-      ),
+      body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            children: <Widget>[
+              SizedBox(height: 80.0),
+              Column(
+                children: <Widget>[
+                  Image.asset('assets/asteroid.png'),
+                  SizedBox(height: 20.0),
+                  Text('Hotfoot'),
+                ],
+              ),
+              SizedBox(height: 65.0),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  filled: true,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              // Password
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  filled: true,
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 15.0),
+              ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: Text('Cancel'),
+                    color: Colors.blue,
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10), 
+                    onPressed: () {
+                    },
+                  ),
+                  Center(child: submitButton(context))
+                ],
+              )
+            ],
+          ),
+        )
     );
   }
 }
@@ -20,5 +61,7 @@ Widget submitButton(context) {
       Navigator.pushNamed(context, '/home');
     },
     child: Text('Login'),
+    color: Colors.amber,
+    padding: EdgeInsets.fromLTRB(10, 10, 10, 10)
   );
 }
