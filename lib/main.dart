9f6b1fed4d07058c68521e7865be1b0e60f8ab73
kattenlaf <ruelgordon123@gmail.com>
2020-02-27 20:18:15 -0500

    13 - Updated yaml file to include new packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
-import 'src/app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hotfoot/src/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:hotfoot/src/utils/user_repository.dart';
+import 'package:hotfoot/src/app.dart';
 
-void main() => runApp(App());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final UserRepository userRepository = UserRepository();
+  runApp(
+    BlocProvider(
+      create: (context) => AuthenticationBloc(userRepository: userRepository)
+        ..add(AppStarted()),
+      child: App(userRepository: userRepository),
+    ),
+  );
+}
+
+// void main() => runApp(App());
 
