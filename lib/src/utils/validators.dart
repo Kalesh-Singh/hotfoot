81f6ae5c2318cfc4671f901418a05939e2a1dad8
kattenlaf <ruelgordon123@gmail.com>
2020-03-03 15:29:47 -0500

    33 - validators now only allow bison.howard.edu

--- lib/src/utils/validators.dart
+++ lib/src/utils/validators.dart
@@ -1,6 +1,6 @@
 class Validators {
   static final RegExp _emailRegExp = RegExp(
-    r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$',
+    r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@bison.howard.edu',
   );
   static final RegExp _passwordRegExp = RegExp(
     r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$',
