55dcfcd28e1381286396cee14135915871626a2f
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-03-05 22:14:54 -0800

    Update debug run test

--- scripts/debug-run.sh
+++ scripts/debug-run.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
 flutter run --no-hot --pid-file pid.txt &> output.txt &
-sleep 120
+sleep 600
 kill "$(< pid.txt)"
-if grep -q "throw" output.txt; then
+
+call_stack=$( grep "call stack" output.txt)
+error=$( grep "error" output.txt )
+
+if [ -n "$call_stack" ] || [ -n "$error" ]; then
     echo "ERROR: "
     echo "$(< output.txt)"
     rm output.txt pid.txt
