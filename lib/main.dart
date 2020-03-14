ceeecb7ad7e159b25adf97a63173e4bbfb3cada5
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-03-14 00:15:22 -0700

    44 - Registered dependencies in service locator

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hotfoot/src/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:hotfoot/src/utils/user_repository.dart';
 import 'package:hotfoot/src/app.dart';
+import 'injection_container.dart' as di;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  di.init();
   final UserRepository userRepository = UserRepository();
   runApp(
     BlocProvider(
