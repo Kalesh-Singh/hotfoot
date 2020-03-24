590e4f40fc1cb851df2c3c3e973b73019dc496e7
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-03-23 18:35:37 -0700

    48 - Network Info Util

--- test/features/places/data/repositories/places_repositories_impl_test.dart
+++ test/features/places/data/repositories/places_repositories_impl_test.dart
@@ -1,20 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hotfoot/features/places/data/repositories/places_repositories_impl.dart';
-import 'package:meta/meta.dart';
-import 'package:mockito/mockito.dart';
 
 void main() {
-  Firestore firestore;
-  PlacesRepository repository;
   setUp(() {
-    firestore = Firestore.instance;
-    repository = PlacesRepository(firestore: firestore);
-  });
-  
-  test('trial', () async {
-    // act
-    repository.getPlacesIds();
   });
 }
