4e4f4d7b26b5a826931f29f66796e953bf2563ee
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-03-25 23:20:15 -0700

    48 - Wired places list to reques_run screen

--- lib/features/places/presentation/ui/widgets/place_card.dart
+++ lib/features/places/presentation/ui/widgets/place_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hotfoot/features/navigation_screen/presentation/bloc/navigation_screen_bloc.dart';
+import 'package:hotfoot/features/navigation_screen/presentation/bloc/navigation_screen_event.dart';
 import 'package:hotfoot/features/places/domain/entities/place_entity.dart';
 import 'package:hotfoot/features/places/presentation/blocs/place_details/place_details_bloc.dart';
 import 'package:hotfoot/features/places/presentation/blocs/place_details/place_details_state.dart';
@@ -13,60 +15,66 @@ class PlaceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-      height: 140,
-      width: double.maxFinite,
-      child: Card(
-        elevation: 5,
-        child: Row(
-          children: <Widget>[
-            Flexible(
-              flex: 2,
-              child: BlocBuilder<PlacePhotoBloc, PlacePhotoState>(
-                builder: (BuildContext context, PlacePhotoState state) {
-                  if (state is PlacePhotoLoadSuccess) {
-                    final photoSize = state.placePhoto.lengthSync();
-                    print('PHOTO SIZE UI: $photoSize');
-                    final photoBytes = state.placePhoto.readAsBytesSync();
-                    return Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Image.memory(
-                        photoBytes,
-                        width: 140,
-                        height: 140,
-                        fit: BoxFit.fill,
-                      ),
-                    );
-                  } else {
-                    return Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Image.asset(
-                        'assets/place-photo-placeholder.png',
-                        width: 140,
-                        height: 140,
-                        fit: BoxFit.fill,
-                      ),
-                    );
-                  }
-                },
-              ),
-            ),
-            Flexible(
-              flex: 3,
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(child: Text(placeEntity.name)),
-                    Expanded(child: Text(placeEntity.address)),
-                  ],
+    return GestureDetector(
+      onTap: () {
+        BlocProvider.of<NavigationScreenBloc>(context)
+            .add(EnteredPurchaseFlow());
+      },
+      child: Container(
+        padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+        height: 140,
+        width: double.maxFinite,
+        child: Card(
+          elevation: 5,
+          child: Row(
+            children: <Widget>[
+              Flexible(
+                flex: 2,
+                child: BlocBuilder<PlacePhotoBloc, PlacePhotoState>(
+                  builder: (BuildContext context, PlacePhotoState state) {
+                    if (state is PlacePhotoLoadSuccess) {
+                      final photoSize = state.placePhoto.lengthSync();
+                      print('PHOTO SIZE UI: $photoSize');
+                      final photoBytes = state.placePhoto.readAsBytesSync();
+                      return Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.memory(
+                          photoBytes,
+                          width: 140,
+                          height: 140,
+                          fit: BoxFit.fill,
+                        ),
+                      );
+                    } else {
+                      return Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Image.asset(
+                          'assets/place-photo-placeholder.png',
+                          width: 140,
+                          height: 140,
+                          fit: BoxFit.fill,
+                        ),
+                      );
+                    }
+                  },
                 ),
               ),
-            )
-          ],
+              Flexible(
+                flex: 3,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(child: Text(placeEntity.name)),
+                      Expanded(child: Text(placeEntity.address)),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
