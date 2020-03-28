e5fabf9d97961b6888f04cac661fd2eb710f7427
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-03-27 19:18:18 -0700

    49 - Fixed keyboard overflow bug

--- lib/features/order_placed/presentation/ui/screen/order_placed_screen.dart
+++ lib/features/order_placed/presentation/ui/screen/order_placed_screen.dart
@@ -10,36 +10,38 @@ class OrderPlacedScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Order Placed')),
-      body: Column(children: <Widget>[
-        LinearProgressIndicator(
-          backgroundColor: Colors.lightBlueAccent,
-          valueColor: new AlwaysStoppedAnimation<Color>(Colors.blue),
-        ),
-        Container(
-          height: MediaQuery.of(context).size.height / 1.5,
-          child: Center(child: Text("Live map being updated here", style: TextStyle(fontSize: 24.0))),
-          color: Colors.lightGreenAccent,
-        ),
-        SizedBox(height: 20),
-        Row(
-          // Should use this here so formatting stays similar across
-          // all screen sizes
-          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(width: 40),
-            Text("Status", style: TextStyle(fontSize: 24.0)),
-            SizedBox(width: 110),
-            OpenCloseChatButton()
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            CancelDeliveryButton(),
-            AcceptDeliveryButton()
-          ],
-        ),
-      ],)
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          LinearProgressIndicator(
+            backgroundColor: Colors.lightBlueAccent,
+            valueColor: new AlwaysStoppedAnimation<Color>(Colors.blue),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height / 1.5,
+            child: Center(child: Text("Live map being updated here", style: TextStyle(fontSize: 24.0))),
+            color: Colors.lightGreenAccent,
+          ),
+          SizedBox(height: 20),
+          Row(
+            // Should use this here so formatting stays similar across
+            // all screen sizes
+            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(width: 40),
+              Text("Status", style: TextStyle(fontSize: 24.0)),
+              SizedBox(width: 110),
+              OpenCloseChatButton()
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              CancelDeliveryButton(),
+              AcceptDeliveryButton()
+            ],
+          ),
+        ],),
+      )
     );
   }
 }
