5df6d69648799f0e3c7e9eec17b20f89eb4ed1ce
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-04-03 18:25:03 -0700

    55 - Runs Local Data Source

---
+++ lib/features/runs/data/data_sources/runs_local_data_source.dart
@@ -0,0 +1,40 @@
+import 'package:hotfoot/features/runs/data/data_sources/data_access_objects/run_dao.dart';
+import 'package:hotfoot/features/runs/data/models/run_model.dart';
+import 'package:meta/meta.dart';
+
+abstract class IRunsLocalDataSource {
+  Future<List<String>> getRunsIds();
+
+  /// Returns null if key is not found
+  Future<RunModel> getRunById({@required String id});
+
+  /// Returns the inserted RunModel
+  Future<void> insertOrUpdateRun({@required RunModel runModel});
+}
+
+class PlacesLocalDataSource implements IRunsLocalDataSource {
+  final IRunDao runDao;
+
+  const PlacesLocalDataSource({
+    @required this.runDao,
+  }) : assert(runDao != null);
+
+  @override
+  Future<RunModel> getRunById({String id}) async {
+    return await runDao.get(id: id);
+  }
+
+  @override
+  Future<List<String>> getRunsIds() async {
+    print('Getting run ids from sembast');
+    List<RunModel> runs = await runDao.getAll();
+    print('Got run ids from sembast');
+    print('Number of runs ${runs.length}');
+    return runs.map((run) => run.id).toList();
+  }
+
+  @override
+  Future<void> insertOrUpdateRun({RunModel runModel}) async {
+    return await runDao.insertOrUpdate(runModel: runModel);
+  }
+}
