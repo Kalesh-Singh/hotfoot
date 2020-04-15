4e5c547aeb75ff93f0d5f34b61e3c0c32865e51a
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-04-15 15:46:45 -0700

    73 - Fixed Home Screen stuck on progress indicator

--- lib/features/navigation_home/presentation/ui/screen/home_screen.dart
+++ lib/features/navigation_home/presentation/ui/screen/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:hotfoot/features/navigation_home/presentation/ui/widgets/home_ta
 import 'package:hotfoot/features/navigation_home/presentation/ui/widgets/orders_tab.dart';
 import 'package:hotfoot/features/navigation_home/presentation/ui/widgets/search_tab.dart';
 import 'package:hotfoot/features/user/presentation/blocs/user_type/user_type_bloc.dart';
+import 'package:hotfoot/features/user/presentation/blocs/user_type/user_type_event.dart';
 import 'package:hotfoot/features/user/presentation/blocs/user_type/user_type_state.dart';
 
 // Run Placed Screen at
@@ -13,30 +14,32 @@ import 'package:hotfoot/features/user/presentation/blocs/user_type/user_type_sta
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final userTypeBloc = BlocProvider.of<UserTypeBloc>(context);
-    print('USER TYPE ON HOME: ${userTypeBloc.state.runtimeType}');
-    if (userTypeBloc.state is UserTypeLoading) {
-      return Container(
-        child: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
-
-    return Container(
-      child: BlocBuilder<NavigationHomeBloc, NavigationHomeState>(
-          builder: (context, state) {
-        switch (state.runtimeType) {
-          case HomeIconTab:
-            return HomeTab();
-          case SearchIconTab:
-            return SearchTab();
-          case OrdersIconTab:
-            return OrdersTab();
-          default:
-            return HomeTab();
+    return BlocBuilder<UserTypeBloc, UserTypeState>(
+      builder: (BuildContext context, UserTypeState state) {
+        if (state is UserTypeLoading) {
+          return Container(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else {
+          return Container(
+            child: BlocBuilder<NavigationHomeBloc, NavigationHomeState>(
+                builder: (context, state) {
+              switch (state.runtimeType) {
+                case HomeIconTab:
+                  return HomeTab();
+                case SearchIconTab:
+                  return SearchTab();
+                case OrdersIconTab:
+                  return OrdersTab();
+                default:
+                  return HomeTab();
+              }
+            }),
+          );
         }
-      }),
+      },
     );
   }
 }
