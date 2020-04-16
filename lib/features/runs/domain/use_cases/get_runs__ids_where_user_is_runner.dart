7f15f1a54be33bc5640a4cf897410db34a7761b9
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-04-16 15:18:27 -0700

    73 - Use cases for getting runs by user type

---
+++ lib/features/runs/domain/use_cases/get_runs__ids_where_user_is_runner.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:hotfoot/core/error/failures.dart';
+import 'package:hotfoot/core/use_cases/use_case.dart';
+import 'package:hotfoot/features/runs/domain/repositories/runs_repository.dart';
+import 'package:meta/meta.dart';
+
+class GetRunsIdsWhereUserIsRunner implements UseCase<List<String>, NoParams> {
+  final IRunsRepository runsRepository;
+
+  const GetRunsIdsWhereUserIsRunner({@required this.runsRepository});
+
+  @override
+  Future<Either<Failure, List<String>>> call(NoParams params) async {
+    return await runsRepository.getRunsIdsWhereUserIsRunner();
+  }
+}
