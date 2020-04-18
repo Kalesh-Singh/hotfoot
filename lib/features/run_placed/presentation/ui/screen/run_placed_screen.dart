4db61dd31ab4efee04cfa93a85c1c524380f0455
kattenlaf <ruelgordon123@gmail.com>
2020-04-18 01:07:30 -0400

    75 - removed comments

--- lib/features/run_placed/presentation/ui/screen/run_placed_screen.dart
+++ lib/features/run_placed/presentation/ui/screen/run_placed_screen.dart
@@ -74,44 +74,5 @@ class RunPlacedScreen extends StatelessWidget {
         }
       ),
     );
-    // return Scaffold(
-    //   appBar: AppBar(title: Text('Run Status')),
-    //   body: SingleChildScrollView(
-    //     child: Column(
-    //       children: <Widget>[
-    //         LinearProgressIndicator(
-    //           backgroundColor: Colors.lightBlueAccent,
-    //           valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-    //         ),
-    //         Container(
-    //           height: MediaQuery.of(context).size.height / 1.5,
-    //           child: Center(
-    //               child: Text("Live map being updated here",
-    //                   style: TextStyle(fontSize: 24.0))),
-    //           color: Colors.lightGreenAccent,
-    //         ),
-    //         SizedBox(height: 20),
-    //         Row(
-    //           // Should use this here so formatting stays similar across
-    //           // all screen sizes
-    //           // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    //           children: [
-    //             SizedBox(width: 40),
-    //             Text("Status", style: TextStyle(fontSize: 24.0)),
-    //             SizedBox(width: 110),
-    //             OpenCloseChatButton(),
-    //           ],
-    //         ),
-    //         Row(
-    //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    //           children: [
-    //             CancelDeliveryButton(),
-    //             AcceptDeliveryButton(),
-    //           ],
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
   }
 }
