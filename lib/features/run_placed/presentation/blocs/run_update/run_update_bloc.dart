d40ab8fc706b0a3b9112a35876a16e0ce428d9f9
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-04-24 00:20:54 -0700

    95 - Run Update Bloc

--- lib/features/run_placed/presentation/blocs/run_update/run_update_bloc.dart
+++ lib/features/run_placed/presentation/blocs/run_update/run_update_bloc.dart
@@ -1,35 +1,15 @@
-import 'package:dartz/dartz.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hotfoot/core/error/failures.dart';
-import 'package:hotfoot/core/use_cases/use_case.dart';
-import 'package:hotfoot/features/places/domain/use_cases/get_places_ids.dart';
-import 'package:hotfoot/features/places/presentation/blocs/places_ids/places_ids_event.dart';
-import 'package:hotfoot/features/places/presentation/blocs/places_ids/places_ids_state.dart';
-import 'package:meta/meta.dart';
+import 'package:hotfoot/features/run_placed/presentation/blocs/run_update/run_update_event.dart';
+import 'package:hotfoot/features/run_placed/presentation/blocs/run_update/run_update_state.dart';
 
-class PlacesIdsBloc extends Bloc<PlacesIdsEvent, PlacesIdsState> {
-  static const String _ERROR_MSG = 'Failed to retrieve places ids';
-  final GetPlacesIds getPlacesIds;
-
-  PlacesIdsBloc({@required this.getPlacesIds});
+class RunUpdateBloc extends Bloc<RunUpdateEvent, RunUpdateState> {
+  @override
+  RunUpdateState get initialState => RunUpdateUninitialized();
 
   @override
-  PlacesIdsState get initialState => PlacesIdsUninitialized();
-
-  @override
-  Stream<PlacesIdsState> mapEventToState(PlacesIdsEvent event) async* {
-    if (event is PlacesRequested) {
-      final failureOrPlacesIds = await getPlacesIds(NoParams());
-      yield* _eitherLoadedOrFailureState(failureOrPlacesIds);
+  Stream<RunUpdateState> mapEventToState(RunUpdateEvent event) async* {
+    if (event is RunUpdated) {
+      yield RunUpdateLoadSuccess(runModel: event.runModel);
     }
-  }
-
-  Stream<PlacesIdsState> _eitherLoadedOrFailureState(
-    Either<Failure, List<String>> failureOrPlacesIds,
-  ) async* {
-    yield failureOrPlacesIds.fold(
-      (failure) => PlacesIdsLoadFailure(message: _ERROR_MSG),
-      (placesIds) => PlacesIdsLoadSuccess(placesIds: placesIds),
-    );
   }
 }
