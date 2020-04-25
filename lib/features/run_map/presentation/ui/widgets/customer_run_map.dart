d92a8cbe5fec0807a5d6afbe9ea42fbb13bccce7
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-04-25 12:53:24 -0700

    99 - Get Runner Location Stream Repository impl

---
+++ lib/features/run_map/presentation/ui/widgets/customer_run_map.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hotfoot/features/location/data/models/location_model.dart';
+import 'package:hotfoot/features/navigation_screen/presentation/bloc/navigation_screen_bloc.dart';
+import 'package:hotfoot/features/run_map/presentation/blocs/runner_location/runner_location_bloc.dart';
+import 'package:hotfoot/features/run_map/presentation/blocs/runner_location/runner_location_event.dart';
+import 'package:hotfoot/features/user/domain/entities/user_entity.dart';
+import 'package:location/location.dart' as DeviceLocation;
+
+class CustomerRunMap extends StatefulWidget {
+  @override
+  _CustomerRunMapState createState() => _CustomerRunMapState();
+}
+
+class _CustomerRunMapState extends State<CustomerRunMap> {
+  GoogleMapController mapController;
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      initialCameraPosition: CameraPosition(
+        target: LatLng(24.142, -110.321),
+        zoom: 15,
+      ),
+      onMapCreated: _onMapCreated,
+    );
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = controller;
+    });
+    _listenForLocationUpdates();
+  }
+
+  void _listenForLocationUpdates() {
+    final locationStream = deviceLocation.onLocationChanged;
+    locationStream.listen((DeviceLocation.LocationData locationData) {
+      final runModel =
+          BlocProvider.of<NavigationScreenBloc>(context).state.runModel;
+      final runnerLocation = LocationModel(
+          lat: locationData.latitude, lng: locationData.longitude);
+      print('DEVICE LOCATION UPDATED');
+      // Forward events to our custom bloc
+      BlocProvider.of<RunnerLocationBloc>(context).add(RunnerLocationUpdated(
+        runModel: runModel,
+        runnerLocation: runnerLocation,
+        userType: UserType.RUNNER,
+        mapController: mapController,
+      ));
+    });
+  }
+}
