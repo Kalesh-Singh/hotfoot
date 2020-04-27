73101d6e9e7a619ae2edcecfe9c93f7a13c6e0a8
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-04-27 14:43:38 -0400

    108 - Fixed location update bug

--- lib/features/run_map/domain/use_cases/insert_or_update_runner_location.dart
+++ lib/features/run_map/domain/use_cases/insert_or_update_runner_location.dart
@@ -2,18 +2,17 @@ import 'package:dartz/dartz.dart';
 import 'package:hotfoot/core/error/failures.dart';
 import 'package:hotfoot/core/use_cases/use_case.dart';
 import 'package:hotfoot/features/location/domain/entities/location_entity.dart';
-import 'package:hotfoot/features/places/domain/entities/place_entity.dart';
 import 'package:hotfoot/features/run_map/domain/repositories/runner_location_repository.dart';
 import 'package:meta/meta.dart';
 
 class InsertOrUpdateRunnerLocation
-    implements UseCase<PlaceEntity, RunnerLocationParams> {
+    implements UseCase<void, RunnerLocationParams> {
   final IRunnerLocationRepository runnerLocationRepository;
 
   const InsertOrUpdateRunnerLocation({@required this.runnerLocationRepository});
 
   @override
-  Future<Either<Failure, PlaceEntity>> call(RunnerLocationParams params) async {
+  Future<Either<Failure, void>> call(RunnerLocationParams params) async {
     return await runnerLocationRepository.insertOrUpdateLocation(
       runId: params.runId,
       runnerLocation: params.runnerLocation,
