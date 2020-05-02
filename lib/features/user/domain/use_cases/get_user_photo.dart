f959710cda1d02c6f85ffeaa157adac5198043e9
Kalesh-Singh <kaleshsingh96@gmail.com>
2020-05-02 10:18:39 -0700

    119 - Get User Photo use case

---
+++ lib/features/user/domain/use_cases/get_user_photo.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
+import 'package:hotfoot/core/error/failures.dart';
+import 'package:hotfoot/core/use_cases/use_case.dart';
+import 'package:hotfoot/features/user/domain/repositories/user_repository.dart';
+import 'package:meta/meta.dart';
+
+class GetUsePhoto implements UseCase<File, NoParams> {
+  final IUserRepository userRepository;
+
+  const GetUsePhoto({@required this.userRepository});
+
+  @override
+  Future<Either<Failure, File>> call(NoParams params) async {
+    return await userRepository.getUserPhoto();
+  }
+}
